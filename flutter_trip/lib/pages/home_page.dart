569660f3f954471f46b30fd1f6d4c7fb57816eab
typ0520 <php12345@163.com>
2019-07-03 14:19:04 +0800

    banner 轮播图

--- flutter_trip/lib/pages/home_page.dart
+++ flutter_trip/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,8 +10,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('home'),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 160,
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return new Image.network(
+                  "http://via.placeholder.com/350x150",
+                  fit: BoxFit.fill,
+                );
+              },
+              itemCount: 3,
+              pagination: new SwiperPagination(),
+            ),
+          ),
+        ],
       ),
     );
   }
