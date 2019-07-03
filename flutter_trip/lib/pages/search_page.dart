cc4474b6ffc85a0821bc899f02acb842d09fd842
typ0520 <php12345@163.com>
2019-07-03 15:43:32 +0800

    post

--- flutter_trip/lib/pages/search_page.dart
+++ flutter_trip/lib/pages/search_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -6,12 +9,46 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String showResult = '';
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('search'),
+      padding: EdgeInsets.only(top: 50),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              fetchPost().then((model) {
+                setState(() {
+                  showResult =
+                      'result: \n icon: ${model.icon}\n hideAppBar: ${model.hideAppBar}';
+                });
+              }, onError: (e) {});
+            },
+            child: Text('点我'),
+          ),
+          Text(showResult)
+        ],
       ),
     );
+  }
+
+  Future<CommonModel> fetchPost() async {
+    final response = await post(
+        'http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    final result = json.decode(response.body);
+    return CommonModel.fromJSON(result);
+  }
+}
+
+class CommonModel {
+  final String icon;
+  final bool hideAppBar;
+
+  CommonModel({this.icon, this.hideAppBar});
+
+  factory CommonModel.fromJSON(Map<String, dynamic> json) {
+    return CommonModel(icon: json['icon'], hideAppBar: json['hideAppBar']);
   }
 }
